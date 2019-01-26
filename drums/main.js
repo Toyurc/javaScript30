f3addf7dcf8310580f814691e33d6d57c3b430eb
Adebayo-Ige <adebayoige.toyosi@gmail.com>
2019-01-26 18:25:09 +0100

    finally works for drums

--- drums/main.js
+++ drums/main.js
@@ -1,23 +1,19 @@
-function play (e){
+
+  function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+  }
+
+  function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    if(!audio) return;
-    audio.currentTime = 0;
-    audio.play();
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
 
     key.classList.add('playing');
-    // key.classList.remove('playing');
-    // key.classList.toggle('playing');
-}
+    audio.currentTime = 0;
+    audio.play();
+  }
 
-function removeTransition (e) {
-    if(e.propertyName !== 'transform') return;
-    this.classList.remove('playing');
-}
-
-window.addEventListener('keydown',play);
-//transition end event
-
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend',removeTransition
-));
+  const keys = Array.from(document.querySelectorAll('.key'));
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  window.addEventListener('keydown', playSound);
