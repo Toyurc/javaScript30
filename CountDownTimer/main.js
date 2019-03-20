7b068e167645cf7f5875c6b84d6f473c1d0fa4d5
Adebayo-Ige <adebayoige.toyosi@gmail.com>
2019-03-20 17:54:47 +0100

    JS30, day 29 Countdown timer

---
+++ CountDownTimer/main.js
@@ -0,0 +1,61 @@
+let countDown;
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]')
+
+function timer(seconds) {
+    //stops running when not in session, and has issues on ios
+    // setInterval(function() {
+    //     seconds--;
+    // },1000);
+
+    //clear any existing timer
+    clearInterval(countDown);
+
+
+    const now = Date.now();
+    const then = now + seconds * 1000;
+    displayTimeLeft(seconds);
+    displayEndTime(then);
+
+    countDown = setInterval(() => {
+        const secondsLeft = Math.round((then - Date.now()) /1000); 
+        //we need to check if we should stop it
+        if(secondsLeft < 0 ){
+            clearInterval(countDown);
+            return;
+        }
+        displayTimeLeft(secondsLeft)
+    }, 1000)
+}
+
+function displayTimeLeft(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const secondsRemain = seconds % 60;
+    const display = `${minutes}:${secondsRemain < 10 ? '0' : ''}${secondsRemain  }`;
+    timerDisplay.textContent = display;
+    document.title = display;
+}
+
+function displayEndTime(timestamp) {
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    const minutes = end.getMinutes();
+    //change to 12 hour clock
+    endTime.textContent = `Be back at ${hour > 12 ? hour - 12 : hour}:${minutes < 10 ? '0' : ''}${minutes}`
+}
+
+function startTimer() {
+    const seconds = parseInt(this.dataset.time);
+    timer(seconds);
+}
+
+buttons.forEach(button =>  button.addEventListener('click', startTimer));
+
+//mad dom sturvs
+document.customForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const mins = this.minutes.value;
+    timer(mins * 60);
+    this.reset();
+})
